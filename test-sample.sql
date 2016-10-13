3d787d8025cad279525c973caad51a0593c085ef
Lucas Donaldson <lad47@duke.edu>
2016-10-13 15:55:53 -0400

    added season scoring title query

--- test-sample.sql
+++ test-sample.sql
@@ -7,3 +7,10 @@ AND ((PlayerInGame.points>9 AND PlayerInGame.assists>9 AND PlayerInGame.rebounds
 	OR (PlayerInGame.points>9 AND PlayerInGame.assists>9 AND PlayerInGame.steals>9 AND PlayerInGame.blocks>9)
 	OR (PlayerInGame.points>9 AND PlayerInGame.steals>9 AND PlayerInGame.rebounds>9 AND PlayerInGame.blocks>9)
 	OR (PlayerInGame.steals>9 AND PlayerInGame.assists>9 AND PlayerInGame.rebounds>9 AND PlayerInGame.blocks>9));
+-- Who won 2015 scoring title
+WITH PointsInSeason(first,last,points) AS 
+(SELECT Player.first_name, Player.last_name, 
+	(SELECT SUM(points) FROM PlayerInGame, Game WHERE player_id=Player.id AND game_id=Game.id AND Game.season_year='2015')
+FROM Player)
+SELECT * FROM PointsInSeason
+WHERE points=(SELECT MAX(points) FROM PointsInSeason); 
